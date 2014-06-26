6c902a6ebd14873ccd52c9ef5a4d78cdc86de1fd
liusiqi43 <me@siqi.fr>
2014-06-26 12:18:36 +0200

    [release] ready for first prod release

--- app/src/main/java/com/siqi/bits/app/ui/InAppPurchaseActivity.java
+++ app/src/main/java/com/siqi/bits/app/ui/InAppPurchaseActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -38,6 +39,7 @@ public class InAppPurchaseActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_app_purchase);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
